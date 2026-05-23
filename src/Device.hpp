bcd19f1e60b614423f6d7f946b6e5bd2b561638d
Preston McGee <106772059+Blitblank@users.noreply.github.com>
2026-05-22 19:48:51 -0700

    Feature: VkDevice Creation (#5)
    * update contributing with goals
    
    * add required extensions to vulkan instance
    
    * add validation layers + debug callback
    
    * add device class which enumerates availble gpu devices
    
    * improve device selection logic
    
    * add logical device creation

---
+++ src/Device.hpp
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include <vulkan/vulkan_raii.hpp>
+
+class Device {
+
+    public:
+
+    Device(vk::raii::Instance* instance);
+    ~Device();
+
+    // assigns a capable gpu vkdevice to physicalDevice 
+    bool selectPhysicalDevice();
+
+    // initializes the logical device
+    bool createLogicalDevice();
+
+    private:
+
+    // gives a device a score to attempt to select the most capable device
+    uint32_t evaluatePhysicalDevice(vk::raii::PhysicalDevice& device);
+
+    vk::raii::Instance* instance_ = nullptr;
+    vk::raii::PhysicalDevice physicalDevice_ = nullptr;
+    vk::raii::Device logicalDevice_ = nullptr;
+    vk::raii::Queue graphicsQueue_ = nullptr;
+
+    // required extensions for the physical device
+    std::vector<const char*> requiredDeviceExtensions_ = { vk::KHRSwapchainExtensionName };
+    
+};
