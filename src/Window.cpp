f8dff685a9cea8f5331299019cc94dff70e434d2
Preston McGee <106772059+Blitblank@users.noreply.github.com>
2026-05-25 17:57:59 -0700

    Feature/Presentation (#7)
    * basic create surface
    
    * fix surface creation on linux
    
    * create swapchain
    
    * create imgae views

--- src/Window.cpp
+++ src/Window.cpp
@@ -2,6 +2,7 @@
 #include "Window.hpp"
 
 #include <SDL3/SDL_events.h>
+#include <iostream>
 
 Window::Window() {
 
@@ -42,4 +43,37 @@ void Window::handleEvent(SDL_Event& event) {
     } else if(event.type == SDL_EVENT_WINDOW_RESTORED) {
         rendering_ = true;
     }
+}
+
+bool Window::createSurface(vk::raii::Instance* instance, vk::raii::SurfaceKHR* surface) {
+
+    if(instance == nullptr) {
+        std::cout << "[" << __FUNCTION__ << ": " << __LINE__ << "] Error: cannot create surface with a null Vulkan instance." << std::endl;
+        return false;
+    }
+
+#ifdef __WIN32
+    vk::Win32SurfaceCreateInfoKHR createInfo {
+        .hinstance = GetModuleHandle(nullptr),
+        .hwnd = (HWND)SDL_GetPointerProperty(SDL_GetWindowProperties(sdlWindow_), "SDL.window.win32.hwnd", nullptr);
+    }
+    surface = instance->createWin32SurfaceKHR(createInfo);
+#else // linux (this technically works on windows too but vulkan gives us an explicit method for WIN32)
+    // its just sdl3 uses the c vulkan api and the app uses the c++ api
+    VkSurfaceKHR cSurface;
+    (void)SDL_Vulkan_CreateSurface(sdlWindow_, static_cast<VkInstance>(**instance), nullptr, &cSurface);
+    *surface = vk::raii::SurfaceKHR(*instance, cSurface);
+#endif // __WIN32
+
+    if(surface != nullptr) {
+        return true;
+    } else {
+        std::cout << "[" << __FUNCTION__ << ": " << __LINE__ << "] Error: unable to create window surface." << std::endl;
+        return false;
+    }
+}
+
+bool Window::getExtent(int32_t* width, int32_t* height) {
+    SDL_GetWindowSizeInPixels(sdlWindow_, width, height);
+    return true;
 }
